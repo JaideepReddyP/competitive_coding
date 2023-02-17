a5d7014f8d62ec9edf8fed8b097a89d01df05181
Jaideep Reddy <jaideep.reddy@byjus.com>
2023-02-17 12:27:57 +0530

    simple find max, greedy

--- leetcode/jump_game.cpp
+++ leetcode/jump_game.cpp
@@ -2,26 +2,15 @@
 #include<vector>
 using namespace std;
 
-vector<int> jump;
-
-bool jumping(vector<int>& nums, int n) {
-    if(n == 0) return true;
-    else if(jump[n] != -1) return jump[n];
-    for(int i = 0; i < n; i++) {
-        if(nums[i] >= n-i && jumping(nums, i)){
-            jump[i] = 1;
+bool canJump(vector<int>& nums) {
+    int dis = 0;
+    for (int i = 0; i <= dis; i++) {
+        dis = max(dis, i + nums[i]);
+        if (dis >= nums.size()-1) {
             return true;
         }
     }
-    jump[n] = 0;
     return false;
-}
-
-bool canJump(vector<int>& nums) {
-    int n = nums.size();
-    jump.clear();
-    jump.resize(n, -1);
-    return jumping(nums, n-1);
 }
 
 int main() {
