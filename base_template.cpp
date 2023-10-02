9ab4888e04c0673a9e84e7890a95512e6f6d22c5
Jaideep Reddy <pjaideepr@gmail.com>
2023-10-02 12:56:41 +0530

    cfr901p3 and base template improvements

--- base_template.cpp
+++ base_template.cpp
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
 void solve() {
-    
+      
 }
 
-int main() {
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
     int t;
     cin >> t;
-    while(t--) {
-        solve();
-    }
+    while(t--) solve();
 }
--- codeforces/round 901 div-2/C_Jellyfish_and_Green_Apple.cpp
+++ codeforces/round 901 div-2/C_Jellyfish_and_Green_Apple.cpp
@@ -1,37 +1,18 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
-void cuts(long long n, long long m, long long cnt) {
-    long long temp_value = n/m, temp_count = (1<<cnt);
-    temp_value %= temp_count;
-    double value = (double)temp_value, ans = 0, count = (double)temp_count;
-    // cout << value << ' ' << count << endl;
-    double div = 0.5, c = 2;
-    while(value>0) {
-        // cout << value/count << ' ';
-        if((value/count) >= div) {
-            value -= (count*div);
-            ans += (count*div)*(double)(c-1);
-        }
-        c = c*2;
-        div /= 2;
-    }
-
-    cout << m*ans << '\n';
-    return;
-}
-
-int main() {
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    long long t;
+    int t;
     cin >> t;
     while(t--) {
-        long long n, m;
+        int n, m;
         cin >> n >> m;
 
-        long long cnt = 0;
+        int cnt = 0;
         while(true) {
             if(n%m) {
                 if(m%2) {
